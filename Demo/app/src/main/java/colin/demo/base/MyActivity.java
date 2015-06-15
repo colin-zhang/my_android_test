b44e78caddeab01c589fff558e845ca56ff05cbf
colin-zhang <ingm@foxmail.com>
2015-06-15 16:38:44 +0800

    demo and icons

---
+++ Demo/app/src/main/java/colin/demo/base/MyActivity.java
@@ -0,0 +1,24 @@
+package colin.demo.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by MING on 2015/6/15.
+ */
+public class MyActivity extends Activity {
+    final String TAG = "demo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
---
+++ Demo/app/src/main/java/colin/demo/main/MainActivity.java
@@ -0,0 +1,22 @@
+package colin.demo.main;
+
+/**
+ * Created by MING on 2015/6/15.
+ */
+
+import android.os.Bundle;
+
+import colin.demo.R;
+import colin.demo.base.MyActivity;
+
+/**
+ * Created by MING on 2015/6/15.
+ */
+public  class MainActivity extends MyActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
